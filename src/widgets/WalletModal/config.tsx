16b850b3905dfcd44983d207147664fb5cabc884
DevDev <Hehe@outlook.com>
2021-09-14 11:23:30 +0200

    update config.tsx

--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -1,9 +1,4 @@
 import Metamask from "./icons/Metamask";
-import MathWallet from "./icons/MathWallet";
-import TokenPocket from "./icons/TokenPocket";
-import TrustWallet from "./icons/TrustWallet";
-import WalletConnect from "./icons/WalletConnect";
-import BinanceChain from "./icons/BinanceChain";
 import { Config } from "./types";
 
 const connectors: Config[] = [
