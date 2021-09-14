e8d729d50fc14590bc01bc16b63f334360fcd154
ArbiKiwi <arbikiwifi@gmail.com>
2021-09-14 04:59:17 +0200

    Update index.ts

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -26,3 +26,4 @@ export { default as BondsIcon } from "./Bonds";
 export { default as VaultsIcon } from "./Vaults";
 export { default as EggHouseIcon } from "./EggHouse";
 export { default as KiwiIcon  } from "./Kiwi";
+export { default as JuiceIcon  } from "./Juice";
